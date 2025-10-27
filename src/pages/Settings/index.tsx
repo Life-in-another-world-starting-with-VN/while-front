7bfb86d4ef33dbd9af28dd6c88b2e1328d708051
Stephan Kim <hhkimstar1226@gmail.com>
2025-10-28 00:11:03 +0900

    chore: 세팅 화면 사이드바 수정

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -160,11 +160,8 @@ const Settings: React.FC<SettingsProps> = ({ onNavigate }) => {
   const navigationItems: NavigationItem[] = useMemo(
     () => [
       { label: '대사록', pageType: 'dialogue' },
-      { label: '저장하기', pageType: 'saveGame' },
-      { label: '불러오기', pageType: 'loadGame' },
       { label: '환경설정', pageType: 'settings', isActive: true },
       { label: '메인 메뉴', pageType: 'mainMenu' },
-      { label: '조작방법', pageType: 'controls' },
       { label: '종료하기', pageType: 'exit' },
     ],
     [],
